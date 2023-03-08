b44b03273ab87a71adf382ed6354a099e2105e7f
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2023-03-08 01:07:00 +0100

    added presenter and layout variables on  mainActivity

---
+++ app/src/main/java/com/example/ujisubjects/mysubjects/MySubjectPresenter.kt
@@ -0,0 +1,14 @@
+package com.example.ujisubjects.mysubjects
+
+import com.example.ujisubjects.MainActivity
+import com.example.ujisubjects.model.UjiSubjectsModel
+
+class MySubjectPresenter(val view: MainActivity) {
+
+    var model : UjiSubjectsModel = UjiSubjectsModel()
+
+    fun onAddSubject() {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/example/ujisubjects/mysubjects/MySubjectsActivity.kt
+++ app/src/main/java/com/example/ujisubjects/mysubjects/MySubjectsActivity.kt
@@ -2,11 +2,27 @@ package com.example.ujisubjects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ujisubjects.mysubjects.MySubjectPresenter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var addSubjectButton: Button
+    lateinit var recyclerViewMySubjects : RecyclerView
+
+    lateinit var presenter : MySubjectPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.title = "My Subjects"
+
+        addSubjectButton = findViewById(R.id.buttonAddSubjects)
+        recyclerViewMySubjects = findViewById(R.id.recyclerViewMySubjects)
+
+        presenter = MySubjectPresenter( this )
+
+        addSubjectButton.setOnClickListener { presenter.onAddSubject() }
     }
 }
